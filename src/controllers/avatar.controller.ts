afb999c94ecb7b759b49bfc35963d12713339b2a
Marko <mmarko.perovici3@gmail.com>
2023-05-07 10:03:46 +0200

    file upload...

--- src/controllers/avatar.controller.ts
+++ src/controllers/avatar.controller.ts
@@ -1,13 +1,38 @@
 import { Request, Response, NextFunction } from "express";
 import { IAuthUser } from "../models/auth-user";
 import { AvatarService } from "../services/avatar-service";
+import { UserService } from "../services/user-service";
+import { HttpError } from "../errors/http-error";
+import { Constants } from "../common/constants";
+import { StatusCodes } from "http-status-codes";
+import path from "path";
+import { Directory, File } from "../utils/file-system";
 
 const _avatarService = new AvatarService();
+const _userService = new UserService();
 
 export class AvatarController {
   public async upload(req: Request, res: Response, next: NextFunction): Promise<void> {
-     const file = req.file;
-     const { userId } = req.user as IAuthUser;
+    const file = req.file;
+    const { userId } = req.user as IAuthUser;
 
+    const entityExist = await _userService.entityExistAsync(userId);
+
+    if (!entityExist) {
+      throw new HttpError(Constants.EndpointNotFound, StatusCodes.NOT_FOUND);
+    }
+
+    const rootPath = path.join(path.resolve("files"), userId.toString());
+    const userDirExist = await Directory.existAsync(rootPath);
+
+    if (!userDirExist) {
+      await Directory.createAsync(rootPath);
+    }
+
+    await File.createAsync(rootPath, file.buffer);
+
+    // await _avatarService.createAsync({
+    //    userId: userId,
+    // });
   }
 }
--- src/utils/file-system.ts
+++ src/utils/file-system.ts
@@ -46,4 +46,40 @@ export class File {
   public static async getInfoAsync(path: string): Promise<any> {
     await fsPromise.stat(path);
   }
+
+  public static async existAsync(filePath: string): Promise<boolean> {
+    const filesPath = path.resolve(filePath);
+
+    try {
+      await fsPromise.access(filesPath);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+}
+
+export class Directory {
+  public static async existAsync(directoryPath: string): Promise<boolean> {
+    const filesPath = path.resolve(directoryPath);
+
+    try {
+      await fsPromise.access(filesPath);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  public static async createAsync(directoryPath: string): Promise<void> {
+     await fsPromise.mkdir(directoryPath);
+  }
+
+  public static async deleteAsync(directoryPath: string): Promise<void> {
+    await fsPromise.rmdir(directoryPath);
+  }
+
+  public static async readAsync(directoryPath: string): Promise<string[]> {
+    return await fsPromise.readdir(directoryPath);
+  }
 }
